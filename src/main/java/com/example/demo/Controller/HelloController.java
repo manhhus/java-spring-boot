59844a72c06240cf40522275ddebe51212c261ac
manhhus <seeagen01@gmail.com>
2024-02-21 22:22:54 +0700

    update features CRUD

--- src/main/java/com/example/demo/Controller/HelloController.java
+++ src/main/java/com/example/demo/Controller/HelloController.java
@@ -1,49 +1,53 @@
-package com.example.demo.Controller;
-
-import jakarta.servlet.ServletContext;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-
-import com.example.demo.entity.User;
-import com.example.demo.utilities.UploadFileUtils;
-
-import org.springframework.ui.Model;
-
-@Controller
-public class HelloController {
-	@Autowired
-	private ServletContext app;
-	
-	@Autowired
-	private UploadFileUtils uploadUtil;
-	@RequestMapping("/hello")
-//	public String hello(Model model){
-////		User user = new User();
-////		user.setName("Nguyen Van A");
-//		
-////		model.addAttribute("myUser", user);
+//package com.example.demo.Controller;
+//
+//import jakarta.servlet.ServletContext;
+//
+//import jakarta.servlet.http.HttpServletRequest;
+//
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import com.example.demo.entity.User;
+//import com.example.demo.utilities.UploadFileUtils;
+//
+//import org.springframework.ui.Model;
+//
+//@Controller
+//public class HelloController {
+//	@Autowired
+//	private HttpServletRequest request;
+//	@Autowired
+//	private ServletContext app;
+//	
+//	@Autowired
+//	private UploadFileUtils uploadUtil;
+//	@RequestMapping("/hello")
+////	public String hello(Model model){
+//////		User user = new User();
+//////		user.setName("Nguyen Van A");
+////		
+//////		model.addAttribute("myUser", user);
+////		return "hello";
+////	}
+//	public String hello() {
 //		return "hello";
 //	}
-	public String hello() {
-		return "hello";
-	}
-	
-
-	@PostMapping("/upload-hello")
-	public String uploadFileHello(
-		@RequestParam("upload_file") List<MultipartFile> uploadedFiles
-	) {
-		for (MultipartFile uploadedFile : uploadedFiles) {
-			this.uploadUtil.handleUploadFile(uploadedFile);
-			System.out.println(this.app.getRealPath(""));
-		}
-		return "redirect:/hello";
-	}
-}
+//	
+//
+//	@PostMapping("/upload-hello")
+//	public String uploadFileHello(
+//		@RequestParam("upload_file") List<MultipartFile> uploadedFiles
+//	) {
+//		for (MultipartFile uploadedFile : uploadedFiles) {
+//			this.uploadUtil.handleUploadFile(uploadedFile);
+//			System.out.println(this.app.getRealPath(""));
+//		}
+//		return "redirect:/hello";
+//	}
+//}
--- src/main/java/com/example/demo/Controller/admin/UserController.java
+++ src/main/java/com/example/demo/Controller/admin/UserController.java
@@ -1,98 +1,6 @@
 package com.example.demo.Controller.admin;
-//
-//import java.util.List;
-//
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//import com.example.demo.dto.UserDTO;
-//import com.example.demo.entity.User;
-//import com.example.demo.mappers.UserMapper;
-//import com.example.demo.repositories.UserRepository;
-//
-//import org.springframework.ui.Model;
-//
-////import org.springframework.web.bind.annotation.RequestParam;
-//
-////import jakarta.servlet.http.HttpServletRequest;
-//
-//@RequestMapping(value = "/admin/users")
-//@Controller
-//public class UserController {
-////	@Autowired
-////	private HttpServletRequest request;
-//	
-//	@GetMapping()
-//	public String index() {
-//		return "admin/users/index";
-//	}
-////test truy van csdl
-////	@Autowired
-////	private UserMapper mapper;
-////	@Autowired
-////	private UserRepository userRepo;
-////	@GetMapping()
-////	public String index(Model model) {
-////		List<User> listUser = this.userRepo.findAll();
-////		model.addAttribute("listUser", listUser);
-////		return "admin/users/index";
-////	}
-//		
-////	@GetMapping(params = "id")
-////	public String show(@RequestParam(name="id") Integer id) {
-//////		this.request.getParameter("id");
-////		System.out.print("id: " + id);
-////		return "admin/users/show";
-////	}
-//	
-//	
-//////	@GetMapping(params = "id")
-////	@GetMapping("/{id}")
-////	public String show(@PathVariable("id") Integer id) {
-//////		this.request.getParameter("id");
-////		System.out.print("id: " + id);
-////		return "admin/users/show";
-////	}
-//	
-////	@GetMapping(value="{id}")
-////	public String show(
-////		Model model,
-////		@PathVariable("id") User entity
-////	) {
-////		UserDTO userDTO = this.mapper.convertToDTO(entity);
-////		model.addAttribute("user", userDTO);
-////
-////		return "admin/users/show";
-////	}
-//	
-//	@GetMapping(value = "/create")
-//	public String create() {
-//		return "admin/users/create";
-//	}
-//	@PostMapping(value = "/store")
-//	public String store() {
-//		return "admin/users/store";
-//	}
-//	@GetMapping(value = "/edit", params = "id")
-//	public String edit() {
-//		return "admin/users/edit";
-//	}
-//	@PostMapping(value = "/update", params = "id")
-//	public String update() {
-//		return "admin/users/update";
-//	}
-//	@PostMapping(value = "/delete", params = "id")
-//	public String delete() {
-//		return "admin/users/delete";
-//	}
-//}
-import java.util.Date;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -126,6 +34,7 @@ import com.example.demo.utilities.HashUtil;
 @Controller
 @RequestMapping(value="/admin/users")
 public class UserController {
+	
 	@Autowired
 	private HttpServletRequest request;
 	
